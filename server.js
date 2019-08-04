898201fa6107f23ec9be1524d15adfe62e26e016
Jan Skowronski <crianonim@gmail.com>
2019-08-04 08:05:34 +0100

    Works with google sign in

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express=require('express');
-const app = express()
+const app = express();
+const verify=require('./verify-google');
 const port = 5500
 
-app.get('/token',(req,res) => {
+app.get('/token',async (req,res) => {
     let id_token=req.query.id_token;
     console.log("IN TOKEN",id_token);
-    res.send('{"a":12}');
+    res.send(await verify(id_token));
 })
 app.get('/', (req, res) => res.sendFile(__dirname+'/index.html'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
